aa78f5604430fc687b470ce42e67dc90bd065832
Cristian <cristianpb94@gmail.com>
2022-11-13 17:20:22 +0100

    Actualizado la build de personajes

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         boolean menu = true;
         int options;
         int options2;
-
+        boolean alreadyPlayer1 = false;
         //Variables creacion de personajes
         String nameWarrior;
         int hpWarrior;
@@ -43,73 +43,116 @@ public class Main {
                     options2 = scanner.nextInt();
                     switch (options2) {
                         case 1:
-                            System.out.println("INTRODUCE EL NOMBRE DEL WARRIOR");
-                            nameWarrior = scanner.next();
-                            System.out.println("INTRODUCE LA VIDA DEL WARRIOR");
-                            hpWarrior = scanner.nextInt();
-                            System.out.println("INTRODUCE LA STAMINA DEL WARRIOR");
-                            staminaWarrior = scanner.nextInt();
-                            System.out.println("INTRODUCE LA FUERZA DEL WARRIOR");
-                            strengthWarrior = scanner.nextInt();
-                            Warrior warrior = new Warrior(nameWarrior, hpWarrior,staminaWarrior,strengthWarrior);
-                             players.add(warrior);
-                             contadorPlayers++;
-                             System.out.println("PERSONAJE " + contadorPlayers + ": WARRIOR CREADO");
-                             System.out.println(warrior);
+                            if (alreadyPlayer1 == true) {
+                                players.clear();
+                                alreadyPlayer1=false;
+                                contadorPlayers=0;
+                                System.out.println("INTRODUCE EL NOMBRE DEL WARRIOR");
+                                nameWarrior = scanner.next();
+                                System.out.println("INTRODUCE LA VIDA DEL WARRIOR");
+                                hpWarrior = scanner.nextInt();
+                                System.out.println("INTRODUCE LA STAMINA DEL WARRIOR");
+                                staminaWarrior = scanner.nextInt();
+                                System.out.println("INTRODUCE LA FUERZA DEL WARRIOR");
+                                strengthWarrior = scanner.nextInt();
+                                Warrior warrior = new Warrior(nameWarrior, hpWarrior, staminaWarrior, strengthWarrior);
+                                players.add(warrior);
+                                contadorPlayers++;
+                                System.out.println("PERSONAJE " + contadorPlayers + ": WARRIOR CREADO");
+                                System.out.println(warrior);
+                                break;
+
+                            } else if(alreadyPlayer1==false){
+                                System.out.println("INTRODUCE EL NOMBRE DEL WARRIOR");
+                                nameWarrior = scanner.next();
+                                System.out.println("INTRODUCE LA VIDA DEL WARRIOR");
+                                hpWarrior = scanner.nextInt();
+                                System.out.println("INTRODUCE LA STAMINA DEL WARRIOR");
+                                staminaWarrior = scanner.nextInt();
+                                System.out.println("INTRODUCE LA FUERZA DEL WARRIOR");
+                                strengthWarrior = scanner.nextInt();
+                                Warrior warrior = new Warrior(nameWarrior, hpWarrior, staminaWarrior, strengthWarrior);
+                                players.add(warrior);
+                                contadorPlayers++;
+                                System.out.println("PERSONAJE " + contadorPlayers + ": WARRIOR CREADO");
+                                System.out.println(warrior);
+                                break;
+                            }
+
+                                case 2:
+                                    if (alreadyPlayer1 == true) {
+                                        players.clear();
+                                        alreadyPlayer1 = false;
+                                        contadorPlayers=0;
+                                        System.out.println("INTRODUCE EL NOMBRE DEL WIZARD");
+                                        nameWizard = scanner.next();
+                                        System.out.println("INTRODUCE LA VIDA DEL WIZARD");
+                                        hpWizard = scanner.nextInt();
+                                        System.out.println("INTRODUCE EL MANA DEL WIZARD");
+                                        manaWizard = scanner.nextInt();
+                                        System.out.println("INTRODUCE LA INTELIGENCIA DEL WIZARD");
+                                        intelligenceWizard = scanner.nextInt();
+                                        Wizard wizard = new Wizard(nameWizard, hpWizard, manaWizard, intelligenceWizard);
+                                        players.add(wizard);
+                                        contadorPlayers++;
+                                        System.out.println("PERSONAJE " + contadorPlayers + ": WIZARD CREADO");
+                                        System.out.println(wizard);
+                                        break;
+                                    } else if (alreadyPlayer1==false){
+                                        System.out.println("INTRODUCE EL NOMBRE DEL WIZARD");
+                                        nameWizard = scanner.next();
+                                        System.out.println("INTRODUCE LA VIDA DEL WIZARD");
+                                        hpWizard = scanner.nextInt();
+                                        System.out.println("INTRODUCE EL MANA DEL WIZARD");
+                                        manaWizard = scanner.nextInt();
+                                        System.out.println("INTRODUCE LA INTELIGENCIA DEL WIZARD");
+                                        intelligenceWizard = scanner.nextInt();
+                                        Wizard wizard = new Wizard(nameWizard, hpWizard, manaWizard, intelligenceWizard);
+                                        players.add(wizard);
+                                        contadorPlayers++;
+                                        System.out.println("PERSONAJE " + contadorPlayers + ": WIZARD CREADO");
+                                        System.out.println(wizard);
+                                    }
+
+                                default:
+                                    System.out.println("SELECCIONA UNA OPCIÓN VÁLIDA");
+                                    break;
+                            }
                             break;
                         case 2:
-                            System.out.println("INTRODUCE EL NOMBRE DEL WIZARD");
-                            nameWizard = scanner.next();
-                            System.out.println("INTRODUCE LA VIDA DEL WIZARD");
-                            hpWizard = scanner.nextInt();
-                            System.out.println("INTRODUCE EL MANA DEL WIZARD");
-                            manaWizard = scanner.nextInt();
-                            System.out.println("INTRODUCE LA INTELIGENCIA DEL WIZARD");
-                            intelligenceWizard = scanner.nextInt();
-                            Wizard wizard = new Wizard(nameWizard, hpWizard,manaWizard,intelligenceWizard);
-                            players.add(wizard);
-                            contadorPlayers++;
-                            System.out.println("PERSONAJE " + contadorPlayers + ": WIZARD CREADO");
-                            System.out.println(wizard);
+
+                            if (contadorPlayers >= 2) {
+                                System.out.println("-------------EMPEZANDO EL COMBATE----------------");
+                                System.out.println();
+                                Utilities.combat(players);
+                                break;
+                            } else {
+                                System.out.println("CREA LOS PERSONAJES ANTES");
+                                break;
+                            }
+                        case 3:
+                            //METODO PARA VER EL LOG DEL COMBATE
+                            System.out.println("LOG DEL COMBATE");
+                            break;
+                        case 4:
+                            System.out.println("PERSONAJES IMPORTADOS");
+                            players = Utilities.importCSV(players);
+                            contadorPlayers = 2;
+                            alreadyPlayer1=true;
+                            System.out.println(players);
+                            break;
+                        case 5:
+                            //METODO COMBATE AUTOMATICO
+                            System.out.println("EMPIEZA COMBATE AUTOMÁTICO");
+                            Utilities.automaticCombat();
+                            break;
+                        case 6:
+                            System.out.println("SALIENDO DEL JUEGO...");
+                            menu = false;
                             break;
                         default:
-                            System.out.println("SELECCIONA UNA OPCIÓN VÁLIDA");
-                            break;
+                            System.out.println("ELIGE UNA OPCIÓN VÁLIDA");
                     }
-                    break;
-                case 2:
-
-                    if (contadorPlayers>=2){
-                        System.out.println("-------------EMPEZANDO EL COMBATE----------------");
-                        System.out.println();
-                        Utilities.combat(players);
-                        break;
-                    }else{
-                        System.out.println("CREA LOS PERSONAJES ANTES");
-                        break;
-                    }
-                case 3:
-                    //METODO PARA VER EL LOG DEL COMBATE
-                    System.out.println("LOG DEL COMBATE");
-                    break;
-                case 4:
-                    System.out.println("PERSONAJES IMPORTADOS");
-                    players = Utilities.importCSV(players);
-                    contadorPlayers= 2;
-                    System.out.println(players);
-                    break;
-                case 5:
-                    //METODO COMBATE AUTOMATICO
-                    System.out.println("EMPIEZA COMBATE AUTOMÁTICO");
-                    Utilities.automaticCombat();
-                    break;
-                case 6:
-                    System.out.println("SALIENDO DEL JUEGO...");
-                    menu = false;
-                    break;
-                default:
-                    System.out.println("ELIGE UNA OPCIÓN VÁLIDA");
             }
         }
     }
-}
